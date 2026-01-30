0d85eeed0bf0d0498c866732f17a44c3aff4b7eb
gwainor <me@gokhan.tr>
2026-01-31 00:22:55 +0300

    Initial commit

---
+++ packages/zion-utils/zion/utils/__init__.py
@@ -0,0 +1,5 @@
+from .module_loading import import_string
+
+__all__ = [
+    "import_string",
+]
---
+++ packages/zion-utils/zion/utils/module_loading.py
@@ -0,0 +1,36 @@
+import sys
+from importlib import import_module
+
+
+def cached_import(module_path: str, class_name: str):
+    if not (
+        (module := sys.modules.get(module_path))
+        and (spec := getattr(module, "__spec__", None))
+        and getattr(spec, "_initializing", False) is False
+    ):
+        module = import_module(module_path)
+
+    return getattr(module, class_name)
+
+
+def import_string(dotted_path: str):
+    """
+    Import a dotted module path and return the attribute/class designated by
+    the last name in the path. Raise ImportError if the import failed.
+    """
+    try:
+        module_path, class_name = dotted_path.rsplit(".", 1)
+    except ValueError as err:
+        raise ImportError(f"{dotted_path} doesn't look like a module path") from err
+
+    try:
+        return cached_import(module_path, class_name)
+    except AttributeError as err:
+        raise ImportError(
+            f'Module "{module_path}" does not define a "{class_name}" attribute/class'
+        ) from err
+
+
+__all__ = [
+    "import_string",
+]
