bd02ab42037ead16127032285f222d7b52f67339
gwainor <me@gokhan.tr>
2026-02-05 11:29:34 +0300

    feat(auth): update user model with pid

--- packages/auth/zion/auth/models/user.py
+++ packages/auth/zion/auth/models/user.py
@@ -1,14 +1,14 @@
 from sqlalchemy import Boolean, String
 from sqlalchemy.orm import Mapped, mapped_column
 from zion.db import DbBaseModel
-from zion.db.mixins import IdMixin, SoftDeleteMixin, TimestampMixin
+from zion.db.mixins import IdMixin, PidMixin, SoftDeleteMixin, TimestampMixin
 
 
-class User(DbBaseModel, IdMixin, TimestampMixin, SoftDeleteMixin):
+class User(DbBaseModel, IdMixin, TimestampMixin, SoftDeleteMixin, PidMixin):
     __tablename__ = "users"
 
+    hashed_password: Mapped[str] = mapped_column(String)
     username: Mapped[str | None] = mapped_column(String(20), default=None, index=True)
     email: Mapped[str | None] = mapped_column(String(50), default=None, index=True)
-    hashed_password: Mapped[str] = mapped_column(String)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, index=True)
     is_email_verified: Mapped[bool] = mapped_column(Boolean, default=False)
